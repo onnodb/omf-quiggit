44b302426e6acf33773e0de92c3e216b1108778b
Onno Broekmans <o.broekmans@lumicks.com>
2020-05-03 21:59:38 +0200

    In "Quiggit mode", create abbreviations for Git commands
    When entering a Git directory, automatically create temporary (i.e.,
    session only, and removed when going out of the Git directory)
    fish abbreviations for all Git commands, as well as aliases.
    
    So, for instance, typing "commit" while in a Git repo directory, will
    now automatically expand to "git commit" upon pressing space, Tab, or
    Enter.
    
    This basically gives us the functionality of *gitsh*!

--- conf.d/quiggit.fish
+++ conf.d/quiggit.fish
@@ -1,5 +1,19 @@
 # Quiggit initializations
 
+# List of "all" Git commands (or at least the ones we bother with here)
+set -g __quiggit_git_commands \
+    config \
+    init clone \
+    add status diff commit notes restore reset rm mv \
+    branch checkout switch merge mergetool log stash tag worktree \
+    fetch pull push remote submodule \
+    show log diff difftool range-diff shortlog describe \
+    apply cherry-pick diff rebase revert \
+    bisect blame \
+    am format-patch send-email request-pull \
+    svn \
+    clean gc reflog filter-branch instaweb archive bundle
+
 function __quiggit_git_root_dir -a dir
     set git_root_dir (command git rev-parse --show-toplevel 2>/dev/null)
     switch $dir
@@ -8,6 +22,13 @@ function __quiggit_git_root_dir -a dir
             return
     end
     return 1
+end
+
+function __quiggit_get_aliases -d 'Returns a list of git alias names'
+    git config -z --get-regexp 'alias\..*' | while read -lz alias command _
+        set alias (string replace 'alias.' '' -- $alias)
+        echo $alias
+    end
 end
 
 function __quiggit_status
@@ -19,10 +40,22 @@ function __quiggit_status
 end
 
 function __quiggit_activate
+    # Running an empty command will now run git-status
     function __quiggit_preexec -e fish_preexec -a cmd
         set cmd (string trim "$cmd")
         if [ "$cmd" = "" ]
             __quiggit_status
+        end
+    end
+
+    # Set up abbreviations for common git commands
+    set -g __quiggit_abbr_created
+    for cmd in $__quiggit_git_commands (__quiggit_get_aliases)
+        if abbr -q $cmd
+            # This abbreviation is already defined. Skip it.
+        else
+            abbr -a -g $cmd "git $cmd"
+            set -a __quiggit_abbr_created $cmd
         end
     end
 
@@ -31,6 +64,12 @@ end
 
 function __quiggit_deactivate
     functions -e __quiggit_preexec
+
+    for cmd in $__quiggit_abbr_created
+        abbr -g -e $cmd
+    end
+
+    set -g -e __quiggit_abbr_created
     set -g -e __quiggit_active
 end
 
